ff68789bc541f35f60e2ef388049a896c4c7e733
Asanshay Gupta <46036121+SuperAce100@users.noreply.github.com>
2024-12-19 00:24:37 -0500

    Changed eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import nextEslintPlugin from '@next/eslint-plugin-next';
+import tseslint from 'typescript-eslint';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +12,20 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", 
+    "next/typescript"
+    
+  ),
+  {
+    plugins: {
+      '@next/next': nextEslintPlugin,
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      // ... any other existing rules ...
+    },
+  },
 ];
 
 export default eslintConfig;
